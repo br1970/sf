2ebc22f641aad942e24f969f42d53d51dd79134c
Ricardo Balduino <balduino@us.ibm.com>
2017-02-23 09:37:45 -0800

    test

--- install.sh
+++ install.sh
@@ -49,12 +49,16 @@ azure servicefabric cluster connect $SERVICEFABRICCLUSTER
 echo "================================================="
 echo "Step 3:  Copy to ImageStore"
 echo "================================================="
-azure servicefabric application package copy dhnotification fabric:ImageStore
+azure servicefabric application package copy dhNotification fabric:ImageStore
+[ $? -eq 0 ] || { echo "Failed to copy to ImageStore, Exiting..."; exit 1; }
+azure servicefabric application package copy dhClient fabric:ImageStore
 [ $? -eq 0 ] || { echo "Failed to copy to ImageStore, Exiting..."; exit 1; }
 echo "================================================="
 echo "Step 4:  Register Application"
 echo "================================================="
 azure servicefabric application type register dhnotification
+[ $? -eq 0 ] || { echo "Failed to register application, Exiting..."; exit 1; }
+azure servicefabric application type register dhclient
 [ $? -eq 0 ] || { echo "Failed to register application, Exiting..."; exit 1; }
 echo "================================================="
 echo "Step 5:  Create Application"
