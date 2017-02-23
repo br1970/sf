a88681bfd1a0a15fe3bfb306bec01440ff180515
Ricardo Balduino <balduino@us.ibm.com>
2017-02-23 13:21:31 -0800

    test

--- install.sh
+++ install.sh
@@ -1,46 +1,7 @@
 #!/bin/bash
 [ -z $BUILD_BUILDNUMBER ] && { echo '$BUILDID is undefined, exiting.'; exit 1; }
 echo "Deploying release to dev for Build ID: $BUILD_BUILDNUMBER"
-echo "================================================="
-echo "Step 1:  Update the manifest versions to: $BUILD_BUILDNUMBER"
-echo "================================================="
-echo "---Updating ApplicationManifest.."
-sed -i "/<ApplicationManifest.*ApplicationTypeVersion/s/ApplicationTypeVersion=\".*\"/ApplicationTypeVersion=\"$BUILD_BUILDNUMBER\"/" dreamhomesf/ApplicationManifest.xml
-[ $? -eq 0 ] || { echo "Failed to update Application Manifest, Exiting..."; exit 1; }
-echo "---Updating ApplicationManifest Completed"
-echo "---Updating ServiceManifestRef.."
-sed -i "/<ServiceManifestRef.*ServiceManifestVersion/s/ServiceManifestVersion=\".*\"/ServiceManifestVersion=\"$BUILD_BUILDNUMBER\"/" dreamhomesf/ApplicationManifest.xml
-[ $? -eq 0 ] || { echo "Failed to update ServiceManifestRef, Exiting..."; exit 1; }
-echo "Updating ServiceManifestRef Completed"
-echo "---Updating ServiceManifest.."
-sed -i "/<ServiceManifest.*Version/s/Version=\".*\"/Version=\"$BUILD_BUILDNUMBER\"/" dreamhomesf/dhNotification/ServiceManifest.xml
-[ $? -eq 0 ] || { echo "Failed to update CodePackage, Exiting..."; exit 1; }
-echo "---Updating ServiceManifest Completed"
-echo "---Updating CodePackage.."
-sed -i "/<CodePackage.*Version/s/Version=\".*\"/Version=\"$BUILD_BUILDNUMBER\"/" dreamhomesf/dhNotification/ServiceManifest.xml
-[ $? -eq 0 ] || { echo "Failed to update CodePackage, Exiting..."; exit 1; }
-echo "---Updating CodePackage Completed"
-echo "---Updating ImageName.."
-sed -i "s/<ImageName>.*<\/ImageName>/<ImageName>balduino\/dhnotificationsf:$BUILD_BUILDNUMBER<\/ImageName>/" dreamhomesf/dhNotification/ServiceManifest.xml
-[ $? -eq 0 ] || { echo "Failed to update ImageName, Exiting..."; exit 1; }
-echo "---Updating ImageName Completed"
-echo "================================================="
-echo "\n"
-echo "================================================="
-echo "---Updating ServiceManifest.."
-sed -i "/<ServiceManifest.*Version/s/Version=\".*\"/Version=\"$BUILD_BUILDNUMBER\"/" dreamhomesf/dhClient/ServiceManifest.xml
-[ $? -eq 0 ] || { echo "Failed to update CodePackage, Exiting..."; exit 1; }
-echo "---Updating ServiceManifest Completed"
-echo "---Updating CodePackage.."
-sed -i "/<CodePackage.*Version/s/Version=\".*\"/Version=\"$BUILD_BUILDNUMBER\"/" dreamhomesf/dhClient/ServiceManifest.xml
-[ $? -eq 0 ] || { echo "Failed to update CodePackage, Exiting..."; exit 1; }
-echo "---Updating CodePackage Completed"
-echo "---Updating ImageName.."
-sed -i "s/<ImageName>.*<\/ImageName>/<ImageName>balduino\/dhclientsf:$BUILD_BUILDNUMBER<\/ImageName>/" dreamhomesf/dhClient/ServiceManifest.xml
-[ $? -eq 0 ] || { echo "Failed to update ImageName, Exiting..."; exit 1; }
-echo "---Updating ImageName Completed"
-echo "================================================="
-echo "\n"
+
 echo "================================================="
 echo "Step 2:  Connect to cluster: $SERVICEFABRICCLUSTER"
 echo "================================================="
